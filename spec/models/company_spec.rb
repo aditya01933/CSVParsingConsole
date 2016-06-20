d597f4428a89c01351c544964580814b22f7482f
aditya01933 <adityatiwari01933@gmail.com>
2016-06-20 15:57:27 +0530

    presence validation specs

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
+RSpec.describe Company, type: :model do
   it "should validates presence of name" do
   end
 end
---
+++ spec/models/operation_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe Operation, type: :model do
+  subject {
+      described_class.new(invoice_num: "Anynumber", invoice_date: DateTime.now,
+                        amount: 25, operation_date: DateTime.now, kind: "anything1;anything2",
+                        status: "anystatus")
+    }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end 
+
+  end
+  it "should validates presence of invoice_num" do
+    subject.invoice_num = nil
+    expect(subject).to_not be_valid 
+  end
+
+  it "should validates presence of invoice_date" do
+    subject.invoice_date = nil
+    expect(subject).to_not be_valid 
+  end
+  it "should validates presence of amount" do
+    subject.amount = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "should validates presence of operation_date" do
+    subject.amount = nil
+    expect(operation_date).to_not be_valid
+  end
+
+  it "should validates presence of kind" do
+     subject.kind = nil
+    expect(operation_date).to_not be_valid
+  end
+
+  it "should validates presence of status" do
+    subject.status = nil
+    expect(operation_date).to_not be_valid
+  end
+      
+end
