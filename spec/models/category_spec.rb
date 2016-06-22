779ab28c64bb5a168244b8519f96ee4a9a170bdd
aditya01933 <adityatiwari01933@gmail.com>
2016-06-22 11:49:28 +0530

    association testing, Category-operation

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   it "should validates presence of name" do
   end
+  context "association" do
+    it { is_expected.to have_many(:operations).through(:category_operations) }
+  end
 end
